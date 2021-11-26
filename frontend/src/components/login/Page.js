7adef44576f786a70635f29088b1418247b1ae07
hetvishukla2001 <64137184+hetvishukla2001@users.noreply.github.com>
2021-11-27 05:12:10 +0530

    untracked node_modules

--- frontend/src/components/login/Page.js
+++
@@ -1,7 +0,0 @@
-export default function Page({ children }) {
-    return (
-        <div className="bg-login-background  bg-center w-screen  flex flex-col justify-between" style={{height: "150vh"}}>
-            {children}
-        </div>
-    );
-}
